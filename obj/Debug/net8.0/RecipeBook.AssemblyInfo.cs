797741a122d7720608c8548c26082625a7647e98
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2026-03-29 16:00:03 +0700

    fixes

--- obj/Debug/net8.0/RecipeBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecipeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f88ddf8c583564ee5b786ea1a0c705030c2665b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdce86c8795e40c92fa66528aca0f9361e07c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
