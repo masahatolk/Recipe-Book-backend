8697c679e74c4ed2ec06a1c741bd80b1d279b9c5
Tolkacheva Maria <141151654+masahatolk@users.noreply.github.com>
2026-03-29 16:08:29 +0700

    исправления с блюдами

--- Domain/DishIngredient.cs
+++ Domain/DishIngredient.cs
@@ -1,11 +1,14 @@
-﻿namespace RecipeBook.Api.Domain;
+﻿using System.Text.Json.Serialization;
 
+namespace RecipeBook.Api.Domain;
 public class DishIngredient
 {
     public Guid DishId { get; set; }
+    [JsonIgnore]
     public Dish? Dish { get; set; }
 
     public Guid ProductId { get; set; }
+    [JsonIgnore]
     public Product? Product { get; set; }
 
     public double Grams { get; set; }
--- obj/Debug/net8.0/RecipeBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/RecipeBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdce86c8795e40c92fa66528aca0f9361e07c5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797741a122d7720608c8548c26082625a7647e98")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
